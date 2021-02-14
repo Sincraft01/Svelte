d30a8f8004479a1e51001f9acd75f7ec281a82f3
Sincraft01 <sincraft01@gmail.com>
2021-02-14 19:39:55 +0000

    com

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,8 @@
 	export let name;
 </script>
 <svelte:head>
-
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=raleway&display=swap" rel="stylesheet">
   <script
     type="module"
     src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.esm.js">
@@ -18,8 +19,20 @@
     href="https://cdn.jsdelivr.net/npm/@ionic/core/css/ionic.bundle.css" />
 
 </svelte:head>
+
+<ion-header>
+<ion-toolbar>
+<ion-grid>
+<ion-row class="justify-content-center">
+
+<ion-img class="ion-justify-content-center" src="https://i.ibb.co/1RdX8rV/Illustration.png" margin="auto"></ion-img>
+</ion-toolbar>
+</ion-header>
 <main>
 <ion-searchbar animated=true></ion-searchbar>
+<ion-card class="font-effect-neon">
+text
+</ion-card>
 </main>
 
 <style>
@@ -27,7 +40,9 @@
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
-		margin: 0 auto;
+        margin: 0 auto;
+        font-family:'raleway',sans-serif;
+        font-size:30px;
 	}
 
 	h1 {
